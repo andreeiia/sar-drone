6d11ce29cef76b220f49b1cf1c54346088c26c5d
andreeiia <ctinaug29@gmail.com>
2026-05-28 12:03:55 +0200

    Create camera_detection.py
    simple detection of the person class using camera stream

---
+++ camera_detection.py
@@ -0,0 +1,29 @@
+from ultralytics import YOLO
+import cv2
+
+# Load a pretrained YOLO model
+model = YOLO("yolo26n.pt")
+
+# Perform object detection on an image on class 0 (person)
+# results = model("https://ultralytics.com/images/bus.jpg", classes = 0)
+
+# Perform object detection on a video stream on class 0 (person)
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+while True:
+    ret, img= cap.read()
+    results = model(img, stream=True, classes = 0)
+
+    for r in results:
+        annotated_frame = r.plot() 
+        cv2.imshow('Webcam', annotated_frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+# end video stream
